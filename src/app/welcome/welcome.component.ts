cba51e6852af8a4f68a46a34e9d7b0727f791303
emilyjspencer <emilyspencer89@hotmail.co.uk>
2023-05-04 20:53:20 +0100

    Welcome page fades in. Adds logo. Clicking on logo causes a fade out effect and takes user to about page

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,10 +1,18 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(4000, style({opacity: 1}))
+      ])
+    ])
+  ]
 })
 export class WelcomeComponent implements OnInit {
 
@@ -14,17 +22,18 @@ export class WelcomeComponent implements OnInit {
   name = '';
 
   ngOnInit(): void {
+    //this.fadeIn();
 
   }
 
   zoomIn() {
     this.state = true;
-
     setTimeout(() => {
-    console.log('zoomIn called')
-    this.route.navigate(["/about"]);
-    }, 3000)
+      console.log('zoomIn called')
+      this.route.navigate(["/about"]);
+      }, 4000)
    }
-  }
 
 
+
+}
