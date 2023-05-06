13b11942b568b03a2dd6ea2afa356c9fdbe9dd8e
emilyjspencer <emilyspencer89@hotmail.co.uk>
2023-05-06 20:25:22 +0100

    Adds more information to the interests page. Removes use of accordion

--- src/app/skills/skills.ts
+++ src/app/skills/skills.ts
@@ -2,18 +2,18 @@ import { ISkill } from "src/interfaces/ISkill"
 
 
 export const FRONTENDDEVELOPMENT: ISkill[] = [
- {title: 'Frontend', description: ['React', 'Angular', 'Vanilla JavaScript', 'RxJS', 'GraphQL']},
+ {title: 'Frontend', description: ['React   ', '   Angular  ', '   Vanilla JavaScript  ', '  RxJS  ', 'GraphQL  ']},
 ]
 
 export const DESIGN = [
-  {title: 'Design' , description: ['SCSS', 'MaterialUI', 'AngularMaterial', 'Figma', 'Storybook']}
+  {title: 'Design' , description: ['SCSS  ',  '  MaterialUI  '  , '  AngularMaterial  ' , '  Figma  ', '  Storybook']}
 ]
 
 export const BACKENDDEVELOPMENT = [
-  {title: 'Backend', description: ['Node','Ruby', 'Rails', 'Java', 'Python']}
+  {title: 'Backend', description: ['  Node' , ' Ruby  ' , 'Rails  ' , '  Java  ', '  Python  ']}
 ]
 
 export const OTHER = [
- {title: 'Other', description: ['SQL', 'Agile']}
+ {title: 'Other', description: ['SQL ', ' Agile']}
 
 ]
