e963ec1ef919626e3003bb6960ac9b7d2fdc166c
emilyjspencer <emilyspencer89@hotmail.co.uk>
2023-05-07 22:24:42 +0100

    Adds hover effect to project cards. Adds London silouhette

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,10 +1,17 @@
 import { Component, ContentChildren, OnInit, QueryList } from '@angular/core';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
-  styleUrls: ['./accordion.component.scss']
+  styleUrls: ['./accordion.component.scss'],
+ animations: []
 })
 export class AccordionComponent {
 
